3206bcd638a35c2993018253e6f0b713f773dfca
spalpeen <oxybimboo@gmail.com>
2022-02-28 09:18:15 +0200

    Lesson 2. Context for templates

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,19 +1,46 @@
 from django.shortcuts import render
+import datetime
 
 
 def main(request):
-    # content = {
-    #     'title': 'Продукты',
-    #     'links_menu': links_menu,
-    #     'same_products': same_products,
-    # }
+    title = 'главная'
+    products = [
+        {
+            "name": "Отличный стул",
+            "desc": "Расположитесь комфортно.",
+            "image_src": "product-1.jpg",
+            "image_href": "/product/1/",
+            "alt": "продукт 1",
+        },
+        {
+            "name": "Стул повышенного качества",
+            "desc": "Не оторваться.",
+            "image_src": "product-2.jpg",
+            "image_href": "/product/2/",
+            "alt": "продукт 2",
+        },
+    ]
+    content = {
+        'title': title,
+        'products': products,
+    }
 
-    return render(request, 'mainapp/index.html')
+    return render(request, 'mainapp/index.html', content)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    title = 'продукты'
+    content = {
+        'title': title,
+    }
+    return render(request, 'mainapp/products.html', content)
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    title = 'контакты'
+    visit_date = datetime.datetime.now()
+    content = {
+        'title': title,
+        'visit_date': visit_date,
+    }
+    return render(request, 'mainapp/contact.html', content)
