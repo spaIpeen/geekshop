d74649a49a88d41ea7fda2a2c5f8ccbf6c7e3639
spalpeen <oxybimboo@gmail.com>
2022-03-03 22:07:55 +0200

    Lesson 3. Django ORM start

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -129,3 +129,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+# Media files
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
