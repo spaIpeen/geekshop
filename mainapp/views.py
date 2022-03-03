311bcb10a301816598b2cfa906ee04714e589357
spalpeen <oxybimboo@gmail.com>
2022-03-03 22:29:49 +0200

    Lesson 3. Urls for app

--- mainapp/views.py
+++ mainapp/views.py
@@ -18,7 +18,7 @@ def main(request):
     return render(request, 'mainapp/index.html', content)
 
 
-def products(request):
+def products(request, pk=None):
     title = 'продукты'
     links_menu = ProductCategory.objects.all()
     same_products = Product.objects.all()
@@ -28,6 +28,8 @@ def products(request):
         'same_products': same_products,
         'media_url': settings.MEDIA_URL,
     }
+    if pk:
+        print(f'User select category: {pk}')
     return render(request, 'mainapp/products.html', content)
 
 
