a6af54619ebb377031f31fd8d2d01cc2a656a37d
spalpeen <oxybimboo@gmail.com>
2022-03-03 22:23:29 +0200

    Lesson 3. Models in context

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,15 +2,17 @@ from django.shortcuts import render
 import datetime
 import json
 import pathlib
+from .models import Product, ProductCategory
+from django.conf import settings
 
 
 def main(request):
     title = 'главная'
-    with open(f'{pathlib.Path().absolute()}/mainapp/templates/mainapp/json/products.json') as f:
-        products = json.load(f)
+    products = Product.objects.all()
     content = {
         'title': title,
         'products': products,
+        'media_url': settings.MEDIA_URL,
     }
 
     return render(request, 'mainapp/index.html', content)
@@ -18,14 +20,13 @@ def main(request):
 
 def products(request):
     title = 'продукты'
-    with open(f'{pathlib.Path().absolute()}/mainapp/templates/mainapp/json/links.json') as f:
-        links_menu = json.load(f)
-    with open(f'{pathlib.Path().absolute()}/mainapp/templates/mainapp/json/same_products.json') as f:
-        same_products = json.load(f)
+    links_menu = ProductCategory.objects.all()
+    same_products = Product.objects.all()
     content = {
         'title': title,
         'links_menu': links_menu,
         'same_products': same_products,
+        'media_url': settings.MEDIA_URL,
     }
     return render(request, 'mainapp/products.html', content)
 
@@ -33,8 +34,21 @@ def products(request):
 def contact(request):
     title = 'контакты'
     visit_date = datetime.datetime.now()
-    with open(f'{pathlib.Path().absolute()}/mainapp/templates/mainapp/json/locations.json') as f:
-        locations = json.load(f)
+    locations = [
+        {"city": "Москва", "phone": "+7-888-888-8888", "email": "info@geekshop.ru", "address": "В пределах МКАД"},
+        {
+            "city": "Екатеринбург",
+            "phone": "+7-777-777-7777",
+            "email": "info_yekaterinburg@geekshop.ru",
+            "address": "Близко к центру",
+        },
+        {
+            "city": "Владивосток",
+            "phone": "+7-999-999-9999",
+            "email": "info_vladivostok@geekshop.ru",
+            "address": "Близко к океану",
+        },
+    ]
     content = {
         'title': title,
         'visit_date': visit_date,
