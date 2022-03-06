41781b06bdf6ce77aa6c71481bb4080047b85fe6
spalpeen <oxybimboo@gmail.com>
2022-03-06 17:15:34 +0200

    Lesson 4. User login and logout

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path("", mainapp.main, name="main"),
     path("products/", include("mainapp.urls", namespace="products")),
     path("contact/", mainapp.contact, name="contact"),
+    path("auth/", include("authapp.urls", namespace="auth")),
 ]
 
 
