331c9ee18bee30fd114b41208c64c6057ba25b53
spalpeen <oxybimboo@gmail.com>
2022-04-04 12:50:20 +0200

    lesson_8. Other CBV.

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -8,12 +8,12 @@ app_name = AdminappConfig.name
 
 urlpatterns = [
     path("", adminapp.admin_main, name="admin_main"),
-    path("users/create/", adminapp.user_create, name="user_create"),
+    path("users/create/", adminapp.UsersCreateView.as_view(), name="user_create"),
     path("users/read/", adminapp.UsersListView.as_view(), name="users"),
-    path("users/update/<int:pk>/", adminapp.user_update, name="user_update"),
-    path("users/delete/<int:pk>/", adminapp.user_delete, name="user_delete"),
+    path("users/update/<int:pk>/", adminapp.UserUpdateView.as_view(), name="user_update"),
+    path("users/delete/<int:pk>/", adminapp.UserDeleteView.as_view(), name="user_delete"),
     path("categories/create/", adminapp.ProductCategoryCreateView.as_view(), name="category_create"),
-    path("categories/read/", adminapp.categories, name="categories"),
+    path("categories/read/", adminapp.ProductCategoryListView.as_view(), name="categories"),
     path("categories/update/<int:pk>/", adminapp.ProductCategoryUpdateView.as_view(), name="category_update"),
     path("categories/delete/<int:pk>/", adminapp.ProductCategoryDeleteView.as_view(), name="category_delete"),
     path("products/create/category/<int:pk>/", adminapp.product_create, name="product_create"),
