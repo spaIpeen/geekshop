fa1da8af3fa75d207d5954d98a9d3b824af18618
spalpeen <oxybimboo@gmail.com>
2022-04-11 15:05:56 +0200

    lesson_9. URLs as regexp

--- adminapp/views.py
+++ adminapp/views.py
@@ -24,40 +24,65 @@ class UsersListView(LoginRequiredMixin, ListView):
     template_name = "adminapp/users.html"
 
 
-class UsersCreateView(LoginRequiredMixin, CreateView):
-    model = ShopUser
-    template_name = "adminapp/user_update.html"
-    success_url = reverse_lazy("admin:users")
-    fields = "__all__"
+@user_passes_test(lambda u: u.is_superuser)
+def user_create(request):
+    title = "пользователи/создание"
+
+    if request.method == "POST":
+        user_form = ShopUserRegisterForm(request.POST, request.FILES)
+        if user_form.is_valid():
+            user_form.save()
+            return HttpResponseRedirect(reverse("admin:users"))
+    else:
+        user_form = ShopUserRegisterForm()
+
+    content = {"title": title, "update_form": user_form, "media_url": settings.MEDIA_URL}
+
+    return render(request, "adminapp/user_update.html", content)
 
 
-class UserUpdateView(LoginRequiredMixin, UpdateView):
-    model = ShopUser
-    template_name = "adminapp/user_update.html"
-    success_url = reverse_lazy("admin:users")
-    fields = "__all__"
+@user_passes_test(lambda u: u.is_superuser)
+def user_update(request, pk):
+    title = "пользователи/редактирование"
 
-    def get_context_data(self, **kwargs):
-        context = super(UserUpdateView, self).get_context_data(**kwargs)
-        context["title"] = "пользователи/редактирование"
-        return context
+    edit_user = get_object_or_404(ShopUser, pk=pk)
+    if request.method == "POST":
+        edit_form = ShopUserAdminEditForm(request.POST, request.FILES, instance=edit_user)
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse("admin:user_update", args=[edit_user.pk]))
+    else:
+        edit_form = ShopUserAdminEditForm(instance=edit_user)
 
+    content = {"title": title, "update_form": edit_form, "media_url": settings.MEDIA_URL}
 
-class UserDeleteView(LoginRequiredMixin, DeleteView):
-    model = ShopUser
-    template_name = "adminapp/user_delete.html"
-    success_url = reverse_lazy("admin:users")
-
-    def delete(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        self.object.is_active = False
-        self.object.save()
-        return HttpResponseRedirect(self.get_success_url())
+    return render(request, "adminapp/user_update.html", content)
 
 
-class ProductCategoryListView(LoginRequiredMixin, ListView):
-    model = ProductCategory
-    template_name = "adminapp/categories.html"
+@user_passes_test(lambda u: u.is_superuser)
+def user_delete(request, pk):
+    title = "пользователи/удаление"
+
+    user = get_object_or_404(ShopUser, pk=pk)
+
+    if request.method == "POST":
+        # user.delete()
+        # Instead delete we will set users inactive
+        user.is_active = False
+        user.save()
+        return HttpResponseRedirect(reverse("admin:users"))
+
+    content = {"title": title, "user_to_delete": user, "media_url": settings.MEDIA_URL}
+
+    return render(request, "adminapp/user_delete.html", content)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def categories(request):
+    title = "админка/категории"
+    categories_list = ProductCategory.objects.all()
+    content = {"title": title, "objects": categories_list, "media_url": settings.MEDIA_URL}
+    return render(request, "adminapp/categories.html", content)
 
 
 class ProductCategoryCreateView(LoginRequiredMixin, CreateView):
--- authapp/urls.py
+++ authapp/urls.py
@@ -1,12 +1,13 @@
-from django.urls import path
+from django.urls import re_path
 
 import authapp.views as authapp
+from authapp.apps import AuthappConfig
 
-app_name = "authapp"
+app_name = AuthappConfig.name
 
 urlpatterns = [
-    path("login/", authapp.login, name="login"),
-    path("logout/", authapp.logout, name="logout"),
-    path("register/", authapp.register, name="register"),
-    path("edit/", authapp.edit, name="edit"),
+    re_path(r"^login/$", authapp.login, name="login"),
+    re_path(r"^logout/$", authapp.logout, name="logout"),
+    re_path(r"^register/$", authapp.register, name="register"),
+    re_path(r"^edit/$", authapp.edit, name="edit"),
 ]
