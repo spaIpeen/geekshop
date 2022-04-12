0e44872a438c42cb15ad493220f03a005b57bdbf
spalpeen <oxybimboo@gmail.com>
2022-04-12 15:27:30 +0200

    Lesson 9. Code style fix

--- authapp/views.py
+++ authapp/views.py
@@ -78,7 +78,13 @@ def send_verify_mail(user):
     \n{settings.DOMAIN_NAME}{verify_link}"
 
     print(f"from: {settings.EMAIL_HOST_USER}, to: {user.email}")
-    return send_mail(title, message, settings.EMAIL_HOST_USER, [user.email], fail_silently=False,)
+    return send_mail(
+        title,
+        message,
+        settings.EMAIL_HOST_USER,
+        [user.email],
+        fail_silently=False,
+    )
 
 
 def verify(request, email, activation_key):
