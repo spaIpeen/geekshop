80054cac8501742628ec2ea4307b8728cafeec02
spaIpeen <84104057+spaIpeen@users.noreply.github.com>
2022-04-15 09:50:36 +0200

    Lesson 9 (#9)
    lesson_9. Lesson 9 ready

--- authapp/urls.py
+++ authapp/urls.py
@@ -1,12 +1,14 @@
-from django.urls import path
+from django.urls import re_path
 
 import authapp.views as authapp
+from authapp.apps import AuthappConfig
 
-app_name = "authapp"
+app_name = AuthappConfig.name
 
 urlpatterns = [
-    path("login/", authapp.login, name="login"),
-    path("logout/", authapp.logout, name="logout"),
-    path("register/", authapp.register, name="register"),
-    path("edit/", authapp.edit, name="edit"),
+    re_path(r"^login/$", authapp.login, name="login"),
+    re_path(r"^logout/$", authapp.logout, name="logout"),
+    re_path(r"^register/$", authapp.register, name="register"),
+    re_path(r"^edit/$", authapp.edit, name="edit"),
+    re_path(r"^verify/(?P<email>.+)/(?P<activation_key>\w+)/$", authapp.verify, name="verify"),
 ]
--- geekshop/settings.py
+++ geekshop/settings.py
@@ -69,9 +69,10 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-            ]
+                "mainapp.context_processors.basket",
+            ],
         },
-    }
+    },
 ]
 
 WSGI_APPLICATION = "geekshop.wsgi.application"
@@ -89,18 +90,26 @@ DATABASES = {
 
 
 # Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+# https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
-    },
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"
-    },
-]
+if not DEBUG:
+    AUTH_PASSWORD_VALIDATORS = [
+        {
+            "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        },
+        {
+            "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        },
+        {
+            "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        },
+        {
+            "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        },
+    ]
+else:
+    # Set simple password for debug
+    AUTH_PASSWORD_VALIDATORS = []
 
 
 # Internationalization
@@ -140,3 +149,27 @@ AUTH_USER_MODEL = "authapp.ShopUser"
 # Set login path:
 #   https://docs.djangoproject.com/en/2.2/ref/settings/#login-url
 LOGIN_URL = "authapp:login"
+
+DOMAIN_NAME = "http://localhost:8000"
+
+# Read about sending email:
+#   https://docs.djangoproject.com/en/2.2/topics/email/
+
+# Full list of email settings:
+#   https://docs.djangoproject.com/en/2.2/ref/settings/#email
+EMAIL_HOST = "localhost"
+EMAIL_PORT = "25"
+
+EMAIL_USE_SSL = False
+# If server support TLS:
+# EMAIL_USE_TLS = True
+
+# EMAIL_HOST_USER = "django@geekshop.local"
+# EMAIL_HOST_PASSWORD = "geekshop"
+# For debugging: python -m smtpd -n -c DebuggingServer localhost:25
+EMAIL_HOST_USER = None
+EMAIL_HOST_PASSWORD = None
+
+# Email as files
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = "tmp/email-messages/"
