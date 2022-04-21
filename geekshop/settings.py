7fa465518de6c45276c6793f20697bfd4f21edaa
spalpeen <oxybimboo@gmail.com>
2022-04-21 11:48:19 +0200

    lesson_11. Ordersapp

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "basketapp",
     "adminapp",
     "social_django",
+    "ordersapp",
 ]
 
 MIDDLEWARE = [
@@ -74,6 +75,7 @@ TEMPLATES = [
                 "mainapp.context_processors.basket",
                 "social_django.context_processors.backends",
                 "social_django.context_processors.login_redirect",
+                "django.template.context_processors.media",
             ],
         },
     },
