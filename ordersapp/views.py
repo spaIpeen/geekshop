4d56ed23f02094bf198242e093e31a853ef4c839
spalpeen <oxybimboo@gmail.com>
2022-04-27 19:05:15 +0200

    Lesson 11. Code style fix

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -2,16 +2,16 @@ from django.db import transaction
 from django.db.models.signals import pre_delete, pre_save
 from django.dispatch import receiver
 from django.forms import inlineformset_factory
+from django.http import JsonResponse
 from django.shortcuts import HttpResponseRedirect, get_object_or_404
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 from django.views.generic.detail import DetailView
-from django.http import JsonResponse
 
 from basketapp.models import Basket
+from mainapp.models import Product
 from ordersapp.forms import OrderItemForm
 from ordersapp.models import Order, OrderItem
-from mainapp.models import Product
 
 
 class OrderList(ListView):
